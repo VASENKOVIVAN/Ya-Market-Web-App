06fb45d214ef5d4183fd77232ad1f0d9b788a354
Vasenkov Ivan <swayket@yandex.ru>
2023-01-10 09:48:22 +0300

    v.1

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ import openpyxl
 from flask_cors import CORS
 from flask import *
 from collections import Counter
-
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 cors = CORS(app)
-
-
-
-
 
 UPLOAD_FOLDER = 'C:/Users/79858/Documents/Flask-App-F/static/files/'
 DOWL_FOLDER = 'C:/Users/79858/Documents/Flask-App-F/templates/'
@@ -28,6 +25,9 @@ DOWL_FOLDER = 'C:/Users/79858/Documents/Flask-App-F/templates/'
 ALLOWED_EXTENSIONS = set(['pdf', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# df = pd.read_excel('C:/Users/79858/Documents/Flask-App-F/static/files/tttfirst_mile_shipment_orders_41681422_2022-12-21-2.xlsx')
+# result = df.sort_values('testtw')
+# print(df.head)
 # app.config['C:\Users\79858\Documents\Flask-App-F\UPLOAD_FOLDER']
 
 menu = [
@@ -63,11 +63,8 @@ def upload_file():
 def uploader_file():
     if request.method == 'POST':
 
-        
-
         folder_files_saving = "C:/Users/79858/Documents/Flask-App-F/static/files/"
         folder_files_data = "C:/Users/79858/Documents/Flask-App-F/static/data/"
-
 
         f = request.files['file']
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
@@ -102,18 +99,25 @@ def uploader_file():
         c = Counter(musical_notes)
         print("И ЧО??????????? - ", c)
 
+        searched_pages_on_pdf = []
 
-        # Массив в котором ищу на какой странице этот ярлык
+        # Цикл в котором пробегаю по столбцу с номерами заказов из таблицы экспорта
         for i in range(3, sheet_export_ya.max_row+1):
+            # Переменная, хранящая номер заказа из таблицы экспорта
             search_sku_in_sheet_ya = sheet_export_ya[i][1].value
+            # Цикл в котором пробегаю по каждой странице в pdf и ищу на какой странице этот номер заказа
             for current_page in range(len(pdf)):
                 page = pdf.load_page(current_page)
-                
+                # Если я нашел страницу на которой этот номер заказа
                 if page.search_for(search_sku_in_sheet_ya):
                     print('\n' + '%s найдено на %i странице' % (search_sku_in_sheet_ya, current_page + 1))
-
+                    searched_pages_on_pdf.append(current_page)
+                    print("Найдены страницы: ", searched_pages_on_pdf)
+                    # Беру sku этого заказа и ищу его в базе транслейта
                     for j in range(2, sheet_sku_data_base.max_row+1):
+                        # Если я нашел этот sku в базе, то пишу его на странице pdf
                         if (sheet_export_ya[i][3].value) == (sheet_sku_data_base[j][1].value):
+
                             # print(
                             #     "Это 1: ", (sheet_export_ya[i][3].value),"\n",
                             #     "Это 2: ", (sheet_sku_data_base[j][1].value),"\n",
@@ -134,60 +138,64 @@ def uploader_file():
                                         # (sheet_export_ya[i+1][1].value)
                                     )
 
-                            if i < sheet_export_ya.max_row+1:
-                                if sheet_export_ya[i][1].value == sheet_export_ya[i+1][1].value:
-                                    
-                                    can.drawString(
-                                        120, 
-                                        -338, 
-                                        "ДА - " + str((sheet_sku_data_base[j][2].value)) + 
-                                        ' (' + 
-                                        str(int(sheet_export_ya[i][5].value)) + 
-                                        'pcs)' +
-                                        str(sheet_export_ya[i+1][3].value) + 
-                                        ' (' +
-                                        str(sheet_export_ya[i+1][5].value) + 
-                                        'pcs)'
-                                    )
-                                    print(
-                                        "ДА - " + str((sheet_sku_data_base[j][2].value)) + 
-                                        ' (' + 
-                                        str(int(sheet_export_ya[i][5].value)) + 
-                                        'pcs)' +
-                                        str(sheet_export_ya[i+1][3].value) + 
-                                        ' (' +
-                                        str(sheet_export_ya[i+1][5].value) + 
-                                        'pcs)'
-                                    )
-                                    i += 1
-                                else:
-                                    can.drawString(
-                                        120, 
-                                        -338, 
-                                        str((sheet_sku_data_base[j][2].value)) + 
-                                        ' (' + 
-                                        str(int(sheet_export_ya[i][5].value)) + 
-                                        'pcs)'
-                                    )
-                                    print(
-                                        str((sheet_sku_data_base[j][2].value)) + 
-                                        ' (' + 
-                                        str(int(sheet_export_ya[i][5].value)) + 
-                                        'pcs)'
-                                    )
+                            can.drawString(
+                                120, 
+                                -338, 
+                                str((sheet_sku_data_base[j][2].value)) + 
+                                ' (' + 
+                                str(int(sheet_export_ya[i][5].value)) + 
+                                'pcs)'
+                            )
+                            print(
+                                str((sheet_sku_data_base[j][2].value)) + 
+                                ' (' + 
+                                str(int(sheet_export_ya[i][5].value)) + 
+                                'pcs)'
+                            )
 
-                                can.save()
+                            can.save()
 
-                                #move to the beginning of the StringIO buffer
-                                packet1.seek(0)
-                                new_pdf1 = PdfFileReader(packet1)
-                                
-                                # add the "watermark" (which is the new pdf) on the existing page
-                                page = existing_pdf.getPage(current_page)
-                                page.mergePage(new_pdf1.getPage(0))
-                                output.addPage(page)
-                # break
+                            #move to the beginning of the StringIO buffer
+                            packet1.seek(0)
+                            new_pdf1 = PdfFileReader(packet1)
                             
+                            # add the "watermark" (which is the new pdf) on the existing page
+                            page = existing_pdf.getPage(current_page)
+                            page.mergePage(new_pdf1.getPage(0))
+                            output.addPage(page)
+                # else:
+                #     page = existing_pdf.getPage(current_page)
+                #     page.mergePage(new_pdf1.getPage(0))
+                #     output.addPage(page)
+
+
+        for current_page in range(len(pdf)):
+            # for searched_pages in searched_pages_on_pdf:
+            if current_page not in searched_pages_on_pdf:
+                print("Этого нет:",  current_page)
+
+                packet3 = io.BytesIO()
+                can = canvas.Canvas(packet3, pagesize=letter)
+                pdfmetrics.registerFont(TTFont('Roboto', 'Roboto-Medium.ttf'))
+                can.setFont('Roboto', 6)
+                can.rotate(90)
+
+                can.drawString(
+                    120, 
+                    -338,
+                    'Нет данных'
+                )
+
+                can.save()
+
+                #move to the beginning of the StringIO buffer
+                packet3.seek(1)
+                new_pdf3 = PdfFileReader(packet3)
+
+                page = existing_pdf.getPage(current_page)
+                page.mergePage(new_pdf3.getPage(0))
+                output.addPage(page)
+
 
         # finally, write "output" to a real file
         outputStream = open("addedindexes.pdf", "wb")
